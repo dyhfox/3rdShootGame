8d83d79d5beb2939845321fb8628c87ff12b3f95
kewo <kewo>
2020-09-07 17:51:49 +0800

    调试动画

---
+++ Assets/script/HUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    private static HUD _ins;
+    public static HUD ins{
+        get {
+            if (!_ins){
+                _ins = new HUD();
+            }
+            return _ins;
+        }
+        set {
+            _ins = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
